6a2e8b7a78fbe927f30eac76e0d4cf4a76281a8b
Michael Hoyle <hoylemd@gmail.com>
2014-04-08 21:53:24 -0400

    Added unit class

---
+++ unit.py
@@ -0,0 +1,27 @@
+import model_profile
+
+class Unit:
+    def __init__(self, name, type):
+        self.name = name
+        self.models = []
+        self.leader = None
+        self.type = type
+
+    def addModel(self, model, count):
+        record = {'profile': model, 'count': count}
+        self.models.append(record)
+
+        if self.leader:
+            if model.leadership > self.leader['profile'].leadership:
+                self.leader = record
+        else:
+            self.leader = record
+
+    def print_summary
+
+    def clone(self):
+        new = Unit(self.name)
+        for model in self.models:
+            new.addModel(model['profile'], model['count'])
+
+        return new
